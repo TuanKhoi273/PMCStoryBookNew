17b6a0b69d6a20ad2bf367bfdb7bc8fa93de299d
tuankhoi273@gmail.com <Babyimreal96>
2021-07-09 17:21:39 +0700

    init

---
+++ storybook/storyLoader.js
@@ -0,0 +1,35 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require("../src/component/Common/0-Introduction.stories");
+  require("../src/component/Common/1-Colors.stories");
+  require("../src/component/Common/2-FontText.stories");
+  require("../src/component/Common/3-Avatar.stories");
+  require("../src/component/Common/4-Button.stories");
+  require("../src/component/Common/5-FloatButton.stories");
+  require("../src/component/Common/6-Input.stories");
+  require("../src/component/Common/7-Label.stories");
+  require("../src/component/Common/8-RadioButton.stories");
+  require("../src/component/Common/9-TextButton.stories");
+}
+
+const stories = [
+  "../src/component/Common/0-Introduction.stories",
+  "../src/component/Common/1-Colors.stories",
+  "../src/component/Common/2-FontText.stories",
+  "../src/component/Common/3-Avatar.stories",
+  "../src/component/Common/4-Button.stories",
+  "../src/component/Common/5-FloatButton.stories",
+  "../src/component/Common/6-Input.stories",
+  "../src/component/Common/7-Label.stories",
+  "../src/component/Common/8-RadioButton.stories",
+  "../src/component/Common/9-TextButton.stories",
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
